607d1465a921bab158651682f4de4f2f956c91da
nguyen cuong <cuong0411@outlook.com>
2022-04-07 14:42:51 +0700

    giới thiệu về kế thừa trong java

---
+++ Lap trinh huong doi tuong voi JAVA/InheritanceDemo/src/ConNguoi.java
@@ -0,0 +1,14 @@
+public class ConNguoi {
+    private String cccd;
+    private String hoTen;
+
+    public ConNguoi() {
+        this.cccd = "01006805";
+        this.hoTen = "nguyen an  ";
+    }
+
+    public void output() {
+        System.out.print(this.cccd + " | " + this.hoTen);
+    }
+
+}
