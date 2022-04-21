c33e92c549325d846af6539e8a4a46d1d53214e5
nguyen cuong <cuong0411@outlook.com>
2022-04-21 15:51:51 +0700

    windows forms phuong trinh bac 2

---
+++ LapTrinhVBdotNet/VBdotNetTrenLop/WindowsAppGiaiPhuongTrinhBac2/Form1.Designer.vb
@@ -0,0 +1,221 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormGiaiPhuongTrinhBac2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormGiaiPhuongTrinhBac2))
+        Me.ButtonGiai = New System.Windows.Forms.Button()
+        Me.LabelHeSoA = New System.Windows.Forms.Label()
+        Me.TextBoxHeSoA = New System.Windows.Forms.TextBox()
+        Me.LabelHeSoB = New System.Windows.Forms.Label()
+        Me.LabelHeSoC = New System.Windows.Forms.Label()
+        Me.LabelNghiem1 = New System.Windows.Forms.Label()
+        Me.LabelKQ = New System.Windows.Forms.Label()
+        Me.LabelNghiem2 = New System.Windows.Forms.Label()
+        Me.TextBoxHeSoC = New System.Windows.Forms.TextBox()
+        Me.TextBoxNghiem1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxHeSoB = New System.Windows.Forms.TextBox()
+        Me.TextBoxNghiem2 = New System.Windows.Forms.TextBox()
+        Me.TextBoxKQ = New System.Windows.Forms.TextBox()
+        Me.ButtonTiep = New System.Windows.Forms.Button()
+        Me.ButtonThoat = New System.Windows.Forms.Button()
+        Me.LabelTieuDe = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonGiai
+        '
+        Me.ButtonGiai.Location = New System.Drawing.Point(172, 538)
+        Me.ButtonGiai.Name = "ButtonGiai"
+        Me.ButtonGiai.Size = New System.Drawing.Size(119, 71)
+        Me.ButtonGiai.TabIndex = 0
+        Me.ButtonGiai.Text = "Giải"
+        Me.ButtonGiai.UseVisualStyleBackColor = True
+        '
+        'LabelHeSoA
+        '
+        Me.LabelHeSoA.AutoSize = True
+        Me.LabelHeSoA.Location = New System.Drawing.Point(141, 82)
+        Me.LabelHeSoA.Name = "LabelHeSoA"
+        Me.LabelHeSoA.Size = New System.Drawing.Size(140, 25)
+        Me.LabelHeSoA.TabIndex = 1
+        Me.LabelHeSoA.Text = "Nhập hệ số a"
+        '
+        'TextBoxHeSoA
+        '
+        Me.TextBoxHeSoA.Location = New System.Drawing.Point(325, 82)
+        Me.TextBoxHeSoA.Name = "TextBoxHeSoA"
+        Me.TextBoxHeSoA.Size = New System.Drawing.Size(100, 31)
+        Me.TextBoxHeSoA.TabIndex = 2
+        '
+        'LabelHeSoB
+        '
+        Me.LabelHeSoB.AutoSize = True
+        Me.LabelHeSoB.Location = New System.Drawing.Point(141, 167)
+        Me.LabelHeSoB.Name = "LabelHeSoB"
+        Me.LabelHeSoB.Size = New System.Drawing.Size(140, 25)
+        Me.LabelHeSoB.TabIndex = 3
+        Me.LabelHeSoB.Text = "Nhập hệ số b"
+        '
+        'LabelHeSoC
+        '
+        Me.LabelHeSoC.AutoSize = True
+        Me.LabelHeSoC.Location = New System.Drawing.Point(141, 252)
+        Me.LabelHeSoC.Name = "LabelHeSoC"
+        Me.LabelHeSoC.Size = New System.Drawing.Size(139, 25)
+        Me.LabelHeSoC.TabIndex = 4
+        Me.LabelHeSoC.Text = "Nhập hệ số c"
+        '
+        'LabelNghiem1
+        '
+        Me.LabelNghiem1.AutoSize = True
+        Me.LabelNghiem1.Location = New System.Drawing.Point(141, 350)
+        Me.LabelNghiem1.Name = "LabelNghiem1"
+        Me.LabelNghiem1.Size = New System.Drawing.Size(35, 25)
+        Me.LabelNghiem1.TabIndex = 5
+        Me.LabelNghiem1.Text = "x1"
+        '
+        'LabelKQ
+        '
+        Me.LabelKQ.AutoSize = True
+        Me.LabelKQ.Location = New System.Drawing.Point(141, 446)
+        Me.LabelKQ.Name = "LabelKQ"
+        Me.LabelKQ.Size = New System.Drawing.Size(86, 25)
+        Me.LabelKQ.TabIndex = 6
+        Me.LabelKQ.Text = "Kết quả"
+        '
+        'LabelNghiem2
+        '
+        Me.LabelNghiem2.AutoSize = True
+        Me.LabelNghiem2.Location = New System.Drawing.Point(546, 350)
+        Me.LabelNghiem2.Name = "LabelNghiem2"
+        Me.LabelNghiem2.Size = New System.Drawing.Size(35, 25)
+        Me.LabelNghiem2.TabIndex = 7
+        Me.LabelNghiem2.Text = "x2"
+        '
+        'TextBoxHeSoC
+        '
+        Me.TextBoxHeSoC.Location = New System.Drawing.Point(325, 252)
+        Me.TextBoxHeSoC.Name = "TextBoxHeSoC"
+        Me.TextBoxHeSoC.Size = New System.Drawing.Size(100, 31)
+        Me.TextBoxHeSoC.TabIndex = 8
+        '
+        'TextBoxNghiem1
+        '
+        Me.TextBoxNghiem1.Location = New System.Drawing.Point(325, 344)
+        Me.TextBoxNghiem1.Name = "TextBoxNghiem1"
+        Me.TextBoxNghiem1.Size = New System.Drawing.Size(100, 31)
+        Me.TextBoxNghiem1.TabIndex = 9
+        '
+        'TextBoxHeSoB
+        '
+        Me.TextBoxHeSoB.Location = New System.Drawing.Point(325, 161)
+        Me.TextBoxHeSoB.Name = "TextBoxHeSoB"
+        Me.TextBoxHeSoB.Size = New System.Drawing.Size(100, 31)
+        Me.TextBoxHeSoB.TabIndex = 10
+        '
+        'TextBoxNghiem2
+        '
+        Me.TextBoxNghiem2.Location = New System.Drawing.Point(757, 347)
+        Me.TextBoxNghiem2.Name = "TextBoxNghiem2"
+        Me.TextBoxNghiem2.Size = New System.Drawing.Size(100, 31)
+        Me.TextBoxNghiem2.TabIndex = 11
+        '
+        'TextBoxKQ
+        '
+        Me.TextBoxKQ.Location = New System.Drawing.Point(325, 446)
+        Me.TextBoxKQ.Name = "TextBoxKQ"
+        Me.TextBoxKQ.Size = New System.Drawing.Size(532, 31)
+        Me.TextBoxKQ.TabIndex = 12
+        '
+        'ButtonTiep
+        '
+        Me.ButtonTiep.Location = New System.Drawing.Point(441, 538)
+        Me.ButtonTiep.Name = "ButtonTiep"
+        Me.ButtonTiep.Size = New System.Drawing.Size(119, 71)
+        Me.ButtonTiep.TabIndex = 13
+        Me.ButtonTiep.Text = "Tiếp"
+        Me.ButtonTiep.UseVisualStyleBackColor = True
+        '
+        'ButtonThoat
+        '
+        Me.ButtonThoat.Location = New System.Drawing.Point(738, 538)
+        Me.ButtonThoat.Name = "ButtonThoat"
+        Me.ButtonThoat.Size = New System.Drawing.Size(119, 71)
+        Me.ButtonThoat.TabIndex = 14
+        Me.ButtonThoat.Text = "Thoát"
+        Me.ButtonThoat.UseVisualStyleBackColor = True
+        '
+        'LabelTieuDe
+        '
+        Me.LabelTieuDe.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelTieuDe.Location = New System.Drawing.Point(146, 9)
+        Me.LabelTieuDe.Name = "LabelTieuDe"
+        Me.LabelTieuDe.Size = New System.Drawing.Size(711, 58)
+        Me.LabelTieuDe.TabIndex = 15
+        Me.LabelTieuDe.Text = "Giải phương trình bậc 2"
+        '
+        'FormGiaiPhuongTrinhBac2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1024, 699)
+        Me.Controls.Add(Me.LabelTieuDe)
+        Me.Controls.Add(Me.ButtonThoat)
+        Me.Controls.Add(Me.ButtonTiep)
+        Me.Controls.Add(Me.TextBoxKQ)
+        Me.Controls.Add(Me.TextBoxNghiem2)
+        Me.Controls.Add(Me.TextBoxHeSoB)
+        Me.Controls.Add(Me.TextBoxNghiem1)
+        Me.Controls.Add(Me.TextBoxHeSoC)
+        Me.Controls.Add(Me.LabelNghiem2)
+        Me.Controls.Add(Me.LabelKQ)
+        Me.Controls.Add(Me.LabelNghiem1)
+        Me.Controls.Add(Me.LabelHeSoC)
+        Me.Controls.Add(Me.LabelHeSoB)
+        Me.Controls.Add(Me.TextBoxHeSoA)
+        Me.Controls.Add(Me.LabelHeSoA)
+        Me.Controls.Add(Me.ButtonGiai)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FormGiaiPhuongTrinhBac2"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonGiai As Button
+    Friend WithEvents LabelHeSoA As Label
+    Friend WithEvents TextBoxHeSoA As TextBox
+    Friend WithEvents LabelHeSoB As Label
+    Friend WithEvents LabelHeSoC As Label
+    Friend WithEvents LabelNghiem1 As Label
+    Friend WithEvents LabelKQ As Label
+    Friend WithEvents LabelNghiem2 As Label
+    Friend WithEvents TextBoxHeSoC As TextBox
+    Friend WithEvents TextBoxNghiem1 As TextBox
+    Friend WithEvents TextBoxHeSoB As TextBox
+    Friend WithEvents TextBoxNghiem2 As TextBox
+    Friend WithEvents TextBoxKQ As TextBox
+    Friend WithEvents ButtonTiep As Button
+    Friend WithEvents ButtonThoat As Button
+    Friend WithEvents LabelTieuDe As Label
+End Class
---
+++ LapTrinhVBdotNet/VBdotNetTrenLop/WindowsAppGiaiPhuongTrinhBac2/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class FormGiaiPhuongTrinhBac2
+
+    Private Sub TextBoxKiemTraHeSoA_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxHeSoA.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "." And Not e.KeyChar = "-" Then
+            e.Handled = True
+            MessageBox.Show("Chi nhap so: ", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        If e.KeyChar = Chr(Keys.Enter) Then
+            TextBoxHeSoB.Select()
+        End If
+    End Sub
+    Private Sub TextBoxKiemTraHeSoB_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxHeSoB.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "." And Not e.KeyChar = "-" Then
+            e.Handled = True
+            MessageBox.Show("Chi nhap so: ", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        If e.KeyChar = Chr(Keys.Enter) Then
+            TextBoxHeSoC.Select()
+        End If
+    End Sub
+    Private Sub TextBoxKiemTraHeSoC_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxHeSoC.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "." And Not e.KeyChar = "-" Then
+            e.Handled = True
+            MessageBox.Show("Chi nhap so: ", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        If e.KeyChar = Chr(Keys.Enter) Then
+            Call ButtonGiai_Click(sender, e)
+        End If
+    End Sub
+
+    Private Sub FormGiaiPhuongTrinhBac2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBoxNghiem1.Enabled = False
+        TextBoxNghiem2.Enabled = False
+        TextBoxKQ.Enabled = False
+        TextBoxHeSoA.Select()
+    End Sub
+
+    Private Sub ButtonThoat_Click(sender As Object, e As EventArgs) Handles ButtonThoat.Click
+        'Application.Exit()
+
+        Dim re As DialogResult
+        re = MessageBox.Show("Ban co muon dong form?", "Thong bao", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If re = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub ButtonTiep_Click(sender As Object, e As EventArgs) Handles ButtonTiep.Click
+        TextBoxHeSoA.ResetText()
+        TextBoxHeSoB.ResetText()
+        TextBoxHeSoC.ResetText()
+        TextBoxKQ.ResetText()
+        TextBoxNghiem1.ResetText()
+        TextBoxNghiem2.ResetText()
+        TextBoxHeSoA.Select()
+    End Sub
+
+    Private Sub ButtonGiai_Click(sender As Object, e As EventArgs) Handles ButtonGiai.Click
+        Dim a As Double = TextBoxHeSoA.Text
+        Dim b As Double = TextBoxHeSoB.Text
+        Dim c As Double = TextBoxHeSoC.Text
+        Dim x1 As Double
+        Dim x2 As Double
+        Dim delta As Double
+
+        If a = 0 Then
+            If b = 0 Then
+                If c = 0 Then
+                    TextBoxKQ.Text = "Phương trình vô số nghiệm"
+                Else
+                    TextBoxKQ.Text = "Phương trình vô nghiệm"
+                End If
+            Else
+                x1 = -c / b
+                TextBoxKQ.Text = "Phuong trinh co 1 nghiem"
+                TextBoxNghiem1.Text = x1
+            End If
+        Else
+            delta = b * b - 4 * a * c
+            If delta < 0 Then
+                TextBoxKQ.Text = "Phuong trinh vo nghiem"
+            ElseIf delta = 0 Then
+                x1 = -b / (2 * a)
+                TextBoxKQ.Text = "Phuong trinh co 1 nghiem kep"
+                TextBoxNghiem1.Text = x1
+            Else
+                x1 = (-b - Math.Sqrt(delta)) / (2 * a)
+                x2 = (-b + Math.Sqrt(delta)) / (2 * a)
+                TextBoxKQ.Text = "Phuong trinh co 2 nghiem phan biet"
+                TextBoxNghiem1.Text = x1
+                TextBoxNghiem2.Text = x2
+            End If
+        End If
+    End Sub
+
+End Class
