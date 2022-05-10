62fe19ea1620b311f511d4b86994a1ac09e176e6
nguyen cuong <cuong0411@outlook.com>
2022-05-10 09:50:24 +0700

    bai tap listbox, button, acceptedbutton

---
+++ LapTrinhVBdotNet/VBdotNetTrenLop/BaiTapVN2604/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmThaotacso
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelNhap = New System.Windows.Forms.Label()
+        Me.TextBoxNhap = New System.Windows.Forms.TextBox()
+        Me.ButtonCapNhat = New System.Windows.Forms.Button()
+        Me.GroupBoxChucNang = New System.Windows.Forms.GroupBox()
+        Me.ButtonXoaCuoi = New System.Windows.Forms.Button()
+        Me.ButtonXoaDau = New System.Windows.Forms.Button()
+        Me.ButtonXoaDangChon = New System.Windows.Forms.Button()
+        Me.ButtonLeCuoi = New System.Windows.Forms.Button()
+        Me.ButtonChanDau = New System.Windows.Forms.Button()
+        Me.ButtonTangHai = New System.Windows.Forms.Button()
+        Me.ButtonKetThuc = New System.Windows.Forms.Button()
+        Me.ListBoxSoNguyen = New System.Windows.Forms.ListBox()
+        Me.GroupBoxChucNang.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LabelNhap
+        '
+        Me.LabelNhap.Location = New System.Drawing.Point(72, 60)
+        Me.LabelNhap.Name = "LabelNhap"
+        Me.LabelNhap.Size = New System.Drawing.Size(179, 57)
+        Me.LabelNhap.TabIndex = 0
+        Me.LabelNhap.Text = "Nhập số nguyên:"
+        Me.LabelNhap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBoxNhap
+        '
+        Me.TextBoxNhap.Location = New System.Drawing.Point(294, 60)
+        Me.TextBoxNhap.Name = "TextBoxNhap"
+        Me.TextBoxNhap.Size = New System.Drawing.Size(401, 31)
+        Me.TextBoxNhap.TabIndex = 1
+        '
+        'ButtonCapNhat
+        '
+        Me.ButtonCapNhat.Location = New System.Drawing.Point(765, 60)
+        Me.ButtonCapNhat.Name = "ButtonCapNhat"
+        Me.ButtonCapNhat.Size = New System.Drawing.Size(205, 57)
+        Me.ButtonCapNhat.TabIndex = 2
+        Me.ButtonCapNhat.Text = "Cập nhật"
+        Me.ButtonCapNhat.UseVisualStyleBackColor = True
+        '
+        'GroupBoxChucNang
+        '
+        Me.GroupBoxChucNang.Controls.Add(Me.ButtonXoaCuoi)
+        Me.GroupBoxChucNang.Controls.Add(Me.ButtonXoaDau)
+        Me.GroupBoxChucNang.Controls.Add(Me.ButtonXoaDangChon)
+        Me.GroupBoxChucNang.Controls.Add(Me.ButtonLeCuoi)
+        Me.GroupBoxChucNang.Controls.Add(Me.ButtonChanDau)
+        Me.GroupBoxChucNang.Controls.Add(Me.ButtonTangHai)
+        Me.GroupBoxChucNang.Location = New System.Drawing.Point(474, 202)
+        Me.GroupBoxChucNang.Name = "GroupBoxChucNang"
+        Me.GroupBoxChucNang.Size = New System.Drawing.Size(496, 519)
+        Me.GroupBoxChucNang.TabIndex = 3
+        Me.GroupBoxChucNang.TabStop = False
+        Me.GroupBoxChucNang.Text = "Chức năng"
+        '
+        'ButtonXoaCuoi
+        '
+        Me.ButtonXoaCuoi.Location = New System.Drawing.Point(51, 445)
+        Me.ButtonXoaCuoi.Name = "ButtonXoaCuoi"
+        Me.ButtonXoaCuoi.Size = New System.Drawing.Size(408, 59)
+        Me.ButtonXoaCuoi.TabIndex = 5
+        Me.ButtonXoaCuoi.Text = "Xóa phần tử cuối"
+        Me.ButtonXoaCuoi.UseVisualStyleBackColor = True
+        '
+        'ButtonXoaDau
+        '
+        Me.ButtonXoaDau.Location = New System.Drawing.Point(51, 365)
+        Me.ButtonXoaDau.Name = "ButtonXoaDau"
+        Me.ButtonXoaDau.Size = New System.Drawing.Size(408, 59)
+        Me.ButtonXoaDau.TabIndex = 4
+        Me.ButtonXoaDau.Text = "Xóa phần tử đầu"
+        Me.ButtonXoaDau.UseVisualStyleBackColor = True
+        '
+        'ButtonXoaDangChon
+        '
+        Me.ButtonXoaDangChon.Location = New System.Drawing.Point(51, 288)
+        Me.ButtonXoaDangChon.Name = "ButtonXoaDangChon"
+        Me.ButtonXoaDangChon.Size = New System.Drawing.Size(408, 59)
+        Me.ButtonXoaDangChon.TabIndex = 3
+        Me.ButtonXoaDangChon.Text = "Xóa phần tử đang chọn"
+        Me.ButtonXoaDangChon.UseVisualStyleBackColor = True
+        '
+        'ButtonLeCuoi
+        '
+        Me.ButtonLeCuoi.Location = New System.Drawing.Point(51, 203)
+        Me.ButtonLeCuoi.Name = "ButtonLeCuoi"
+        Me.ButtonLeCuoi.Size = New System.Drawing.Size(408, 59)
+        Me.ButtonLeCuoi.TabIndex = 2
+        Me.ButtonLeCuoi.Text = "Chọn số lẻ cuối"
+        Me.ButtonLeCuoi.UseVisualStyleBackColor = True
+        '
+        'ButtonChanDau
+        '
+        Me.ButtonChanDau.Location = New System.Drawing.Point(51, 118)
+        Me.ButtonChanDau.Name = "ButtonChanDau"
+        Me.ButtonChanDau.Size = New System.Drawing.Size(408, 59)
+        Me.ButtonChanDau.TabIndex = 1
+        Me.ButtonChanDau.Text = "Chọn số chẵn đầu"
+        Me.ButtonChanDau.UseVisualStyleBackColor = True
+        '
+        'ButtonTangHai
+        '
+        Me.ButtonTangHai.Location = New System.Drawing.Point(51, 42)
+        Me.ButtonTangHai.Name = "ButtonTangHai"
+        Me.ButtonTangHai.Size = New System.Drawing.Size(408, 59)
+        Me.ButtonTangHai.TabIndex = 0
+        Me.ButtonTangHai.Text = "Tăng mỗi phần tử lên hai"
+        Me.ButtonTangHai.UseVisualStyleBackColor = True
+        '
+        'ButtonKetThuc
+        '
+        Me.ButtonKetThuc.Location = New System.Drawing.Point(77, 750)
+        Me.ButtonKetThuc.Name = "ButtonKetThuc"
+        Me.ButtonKetThuc.Size = New System.Drawing.Size(893, 43)
+        Me.ButtonKetThuc.TabIndex = 4
+        Me.ButtonKetThuc.Text = "Kết thúc"
+        Me.ButtonKetThuc.UseVisualStyleBackColor = True
+        '
+        'ListBoxSoNguyen
+        '
+        Me.ListBoxSoNguyen.FormattingEnabled = True
+        Me.ListBoxSoNguyen.ItemHeight = 25
+        Me.ListBoxSoNguyen.Location = New System.Drawing.Point(77, 202)
+        Me.ListBoxSoNguyen.Name = "ListBoxSoNguyen"
+        Me.ListBoxSoNguyen.Size = New System.Drawing.Size(284, 504)
+        Me.ListBoxSoNguyen.TabIndex = 5
+        '
+        'FrmThaotacso
+        '
+        Me.AcceptButton = Me.ButtonCapNhat
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1067, 805)
+        Me.Controls.Add(Me.ListBoxSoNguyen)
+        Me.Controls.Add(Me.ButtonKetThuc)
+        Me.Controls.Add(Me.GroupBoxChucNang)
+        Me.Controls.Add(Me.ButtonCapNhat)
+        Me.Controls.Add(Me.TextBoxNhap)
+        Me.Controls.Add(Me.LabelNhap)
+        Me.Name = "FrmThaotacso"
+        Me.Text = "Thao tac so"
+        Me.GroupBoxChucNang.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelNhap As Label
+    Friend WithEvents TextBoxNhap As TextBox
+    Friend WithEvents ButtonCapNhat As Button
+    Friend WithEvents GroupBoxChucNang As GroupBox
+    Friend WithEvents ButtonKetThuc As Button
+    Friend WithEvents ListBoxSoNguyen As ListBox
+    Friend WithEvents ButtonXoaCuoi As Button
+    Friend WithEvents ButtonXoaDau As Button
+    Friend WithEvents ButtonXoaDangChon As Button
+    Friend WithEvents ButtonLeCuoi As Button
+    Friend WithEvents ButtonChanDau As Button
+    Friend WithEvents ButtonTangHai As Button
+End Class
---
+++ LapTrinhVBdotNet/VBdotNetTrenLop/BaiTapVN2604/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class FrmThaotacso
+    Private Sub FrmThaotacso_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ButtonCapNhat.Enabled = False
+        TextBoxNhap.Select()
+        'CloseButton()
+
+    End Sub
+    Private Function KiemTraListBox() As Boolean
+        If (ListBoxSoNguyen.Items.Count > 0) Then
+            Return True
+        End If
+        Return False
+    End Function
+    Private Sub CloseButton()
+        Dim e As Boolean = KiemTraListBox()
+        ButtonTangHai.Enabled = e
+        ButtonChanDau.Enabled = e
+        ButtonLeCuoi.Enabled = e
+        ButtonXoaDangChon.Enabled = e
+        ButtonXoaDau.Enabled = e
+        ButtonXoaCuoi.Enabled = e
+    End Sub
+    Private Sub ButtonTangHai_Click(sender As Object, e As EventArgs) Handles ButtonTangHai.Click
+        Dim length As Integer = ListBoxSoNguyen.Items.Count
+        For i As Integer = 0 To length - 1 Step 1
+            ListBoxSoNguyen.Items(i) += 2
+        Next
+    End Sub
+
+    Private Sub ButtonCapNhat_Click(sender As Object, e As EventArgs) Handles ButtonCapNhat.Click
+        ListBoxSoNguyen.Items.Add(TextBoxNhap.Text)
+        TextBoxNhap.ResetText()
+        TextBoxNhap.Select()
+    End Sub
+
+    Private Sub ButtonChanDau_Click(sender As Object, e As EventArgs) Handles ButtonChanDau.Click
+        'Dim length As Integer = ListBoxSoNguyen.Items.Count
+        'Dim flag As Integer = 0
+        'For i As Integer = 0 To length - 1 Step 1
+        '    If ListBoxSoNguyen.Items(i) Mod 2 = 0 Then
+        '        flag = 1
+        '        ListBoxSoNguyen.SelectedIndex = i
+        '        Exit For
+        '    End If
+        'Next
+
+        'If flag = 0 Then
+        '    MessageBox.Show("Khong co phan tu chan trong list", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        'End If
+
+        'co sua bai
+        Dim n As Integer = ListBoxSoNguyen.Items.Count
+        For i As Integer = 0 To n Step 1
+            If Convert.ToInt32(ListBoxSoNguyen.Items(i)) Mod 2 = 0 Then
+                MessageBox.Show(ListBoxSoNguyen.Items(i))
+                ListBoxSoNguyen.SelectedIndex = i
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub TextBoxNhap_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxNhap.KeyPress
+        'chi duoc nhap so
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) And Not e.KeyChar = "-" Then
+            e.Handled = True
+            MessageBox.Show("Chi nhap so: ", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub ButtonLeCuoi_Click(sender As Object, e As EventArgs) Handles ButtonLeCuoi.Click
+        Dim flag As Integer = 0
+        Dim length As Integer = ListBoxSoNguyen.Items.Count
+        For i As Integer = length - 1 To 0 Step -1
+            If ListBoxSoNguyen.Items(i) Mod 2 <> 0 Then
+                flag = 1
+                ListBoxSoNguyen.SelectedIndex = i
+                Exit For
+            End If
+        Next
+
+        If flag = 0 Then
+            MessageBox.Show("Khong co phan tu le trong list", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub ButtonXoaDangChon_Click(sender As Object, e As EventArgs) Handles ButtonXoaDangChon.Click
+        If ListBoxSoNguyen.SelectedItem Is Nothing Then
+            MessageBox.Show("Xin hay chon so de xoa", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            ListBoxSoNguyen.Items.Remove(ListBoxSoNguyen.SelectedItem)
+        End If
+
+    End Sub
+
+    Private Sub ButtonXoaDau_Click(sender As Object, e As EventArgs) Handles ButtonXoaDau.Click
+        Dim length As Integer = ListBoxSoNguyen.Items.Count
+        If length = 0 Then
+            MessageBox.Show("Khong co du lieu", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            ListBoxSoNguyen.Items.RemoveAt(0)
+        End If
+    End Sub
+
+    Private Sub ButtonXoaCuoi_Click(sender As Object, e As EventArgs) Handles ButtonXoaCuoi.Click
+
+        Dim length As Integer = ListBoxSoNguyen.Items.Count
+        If length = 0 Then
+            MessageBox.Show("Khong co du lieu", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            ListBoxSoNguyen.Items.RemoveAt(ListBoxSoNguyen.Items.Count - 1)
+        End If
+    End Sub
+
+    Private Sub ButtonKetThuc_Click(sender As Object, e As EventArgs) Handles ButtonKetThuc.Click
+        Me.Close()
+    End Sub
+
+    Private Sub FrmThaotacso_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim i As DialogResult
+        i = MessageBox.Show("Ban co muon dong form hay khong", "Thong bao", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        If i = DialogResult.No Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub FrmThaotacso_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        Application.Exit()
+    End Sub
+
+    Private Sub TextBoxNhap_TextChanged(sender As Object, e As EventArgs) Handles TextBoxNhap.TextChanged
+        If TextBoxNhap.TextLength > 0 Then
+            ButtonCapNhat.Enabled = True
+        Else
+            ButtonCapNhat.Enabled = False
+        End If
+    End Sub
+End Class
